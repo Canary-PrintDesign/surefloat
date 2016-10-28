822a90bf13b12b6e9c97a9a52ff01537dd123a26
Sinetheta <k.attfield@gmail.com>
2016-10-27 21:52:59 -0700

    Add webpack

--- config.rb
+++ config.rb
@@ -21,6 +21,14 @@ page '/*.txt', layout: false
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
+
+  activate :external_pipeline,
+    name: :webpack,
+    command: build? ?
+      "./node_modules/webpack/bin/webpack.js --bail -p" :
+      "./node_modules/webpack/bin/webpack.js --watch -d --progress --color",
+    source: ".tmp/dist",
+    latency: 1
 end
 
 ###
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    site: './source/javascripts/all.js'
+  },
+
+  resolve: {
+    root: __dirname + '/source/javascripts',
+  },
+
+  output: {
+    path: __dirname + '/.tmp/dist',
+    filename: 'javascripts/[name].js',
+  },
+};
